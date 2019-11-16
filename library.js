6396f416aedebb187ba8e0c29d7b8bbc49454aeb
ARaut9 <ankitraut999@gmail.com>
2019-11-16 18:42:30 +0530

    add functionality to filter and add id property to books

--- library.js
+++ library.js
@@ -5,10 +5,18 @@ const tableBody = document.querySelector('tbody');
 const addBookFormBtn = document.querySelector('.add-book-btn');
 const cancelBookFormBtn = document.querySelector('.cancel-btn');
 const formContainer = document.querySelector('.form-container');
+const bookFilter = document.querySelector('.book-filter');
 
+bookFilter.selectedIndex = 0;
+let filterValue = 'all';
 const books = JSON.parse(localStorage.getItem('books')) || [];
 
 /* Functions */
+
+let counter = 0;
+function getUID() { // to genrate unique id's
+  return (Date.now() + Math.round(Math.random() * 9) + counter++).toString();
+}
 
 function addBook(e) {
   e.preventDefault();
@@ -18,7 +26,7 @@ function addBook(e) {
   const pages = e.target.pages.value;
   const readStatus = false;
 
-  const book = { title, author, pages, readStatus };
+  const book = { id: getUID(), title, author, pages, readStatus };
   books.push(book);
 
   localStorage.setItem('books', JSON.stringify(books));
@@ -27,8 +35,18 @@ function addBook(e) {
   this.reset();
 }
 
+function filterBooks(filterValue) {
+  if (filterValue === 'read') {
+    return books.filter(book => book.readStatus === true);
+  } else if (filterValue === 'not-read') {
+    return books.filter(book => book.readStatus !== true);
+  }
+  return books;
+}
+
 function renderBooks() {
-  tableBody.innerHTML = books.map((book, i) => {
+  const booksToRender = filterBooks(filterValue);
+  tableBody.innerHTML = booksToRender.map((book, i) => {
     return `
       <tr>
         <td>${i + 1}</td>
@@ -36,13 +54,13 @@ function renderBooks() {
         <td>${book.author}</td>
         <td>${book.pages}</td>
         <td>
-          <input type='checkbox' id=${i} data-index=${i} ${books.readStatus ? 'checked' : ''}>
-          <label for=${i}>
+          <input type='checkbox' id=${book.id} data-id=${book.id} ${book.readStatus ? 'checked' : ''}>
+          <label for=${book.id}>
             ${book.readStatus ? '<i class="fas fa-check-square"></i>' : '<i class="far fa-check-square"></i>'}
           <label>
         </td>
         <td>
-          <button data-index=${i}>
+          <button data-id=${book.id}>
             <i class="fas fa-trash-alt"></i>
           </button>
         </td>
@@ -52,13 +70,15 @@ function renderBooks() {
 }
 
 function handleClick(e) {
+  const id = e.target.parentNode.dataset.id;
+  const book = books.find(book => book.id === id);
+
   if (e.target.matches('input')) {
-    const index = e.target.dataset.index;
-    // toggle read status
-    books[index].readStatus = !books[index].readStatus;
+    // toggle read statu
+    book.readStatus = !book.readStatus;
   } else if (e.target.parentNode.matches('button')) {
-    const index = e.target.dataset.index;
     // delete book
+    const index = books.indexOf(book);
     books.splice(index, 1);
   } else {
     return;
@@ -75,6 +95,12 @@ addBookFormBtn.addEventListener('click', () => formContainer.classList.add('show
 cancelBookFormBtn.addEventListener('click', () => {
   formContainer.classList.remove('show');
   addBookForm.reset();
+});
+bookFilter.addEventListener('change', () => {
+  const index = bookFilter.selectedIndex;
+  const options = bookFilter.options;
+  filterValue = options[index].value;
+  renderBooks();
 });
 
 renderBooks();
